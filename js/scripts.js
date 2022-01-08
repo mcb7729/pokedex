368ef5033fbc329dd4d877440c7626dd7c97a58e
mcb7729 <matthewcbroussard@gmail.com>
2022-01-08 17:00:59 -0600

    Update scripts.js
    Fixed syntax issue - changed = to :

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,1 @@
-let pokemonList = [{name = Bulbasaur, height = 7, type = "grass"},{name = Charizard, height = 15, type = "fire"},{name = Blastoise, height = 10, type = "water"}];
+let pokemonList = [{name: Bulbasaur, height: 7, type: "grass"},{name: Charizard, height: 15, type: "fire"},{name: Blastoise, height: 10, type: "water"}];
