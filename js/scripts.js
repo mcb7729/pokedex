af2f54fb52adc7872890640a51cf81a549b276db
mcb7729 <matthewcbroussard@gmail.com>
2022-01-27 15:08:19 -0600

    Update scripts.js
    Created pokemon list and added highlighted big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-let pokemonList = [{name: Bulbasaur, height: 7, type: "grass"},{name: Charizard, height: 15, type: "fire"},{name: Blastoise, height: 10, type: "water"}];
+let pokemonList = [
+  {name: "Bulbasaur", height: 7, type: "grass"},
+  {name: "Charizard", height: 15, type: "fire"},
+  {name: "Blastoise", height: 10, type: "water"}
+  ];
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' \(height:' + pokemonList[i].height + '\) ' +"<br>");
+
+if (pokemonList[i].height > 10)
+document.write("-WOW, That is BIG!!!" +"<br>");
+}
