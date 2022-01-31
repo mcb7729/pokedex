9921cd7181913ba4af19c0a24b415706d94addb1
mcb7729 <matthewcbroussard@gmail.com>
2022-01-31 13:29:17 -0600

    Update scripts.js
    Created IIFE and converted to forEach loops

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,23 @@ let pokemonList = [
   {name: "Blastoise", height: 10, type: "water"}
   ];
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' \(height:' + pokemonList[i].height + '\) ' +"<br>");
+  document.write("<p>" + pokemonList[i].name + ' \(height:' + pokemonList[i].height + '\)' + "</p>");
 
 if (pokemonList[i].height > 10)
-document.write("-WOW, That is BIG!!!" +"<br>");
+document.write("-WOW, That is BIG!!!");
 }
+
+function divide(dividend, divisor) {
+  if (divisor===0) {
+    return "You are trying to divide by 0."
+    }
+    else {
+      let result = dividend / divisor;
+      return result;
+    }
+}
+
+console.log(divide(4, 2));
+console.log(divide(7, 0));
+console.log(divide(1, 4));
+console.log(divide(12, -3));
