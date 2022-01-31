efc1c56dd3b36dd524cf8c97a258c0f6e652b130
mcb7729 <matthewcbroussard@gmail.com>
2022-01-31 14:16:35 -0600

    Update scripts.js
    Created forEach loops and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,49 @@
-let pokemonList = [
-  {name: "Bulbasaur", height: 7, type: "grass"},
-  {name: "Charizard", height: 15, type: "fire"},
-  {name: "Blastoise", height: 10, type: "water"}
-  ];
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write("<p>" + pokemonList[i].name + ' \(height:' + pokemonList[i].height + '\)' + "</p>");
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Bulbasaur", height: 7, type: "grass"},
+    {name: "Charizard", height: 15, type: "fire"},
+    {name: "Blastoise", height: 10, type: "water"}
+    ];
 
-if (pokemonList[i].height > 10)
-document.write("-WOW, That is BIG!!!");
-}
-
-function divide(dividend, divisor) {
-  if (divisor===0) {
-    return "You are trying to divide by 0."
+    function add(pokemon) {
+      if (typeof pokemon === 'object') {
+        pokemonList.push(pokemon);
     }
-    else {
-      let result = dividend / divisor;
-      return result;
     }
-}
 
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
+    function getAll() {
+      return pokemonList;
+    }
+
+    return {
+      add: add,
+      getAll: getAll,
+    };
+
+})();
+
+pokemonRepository.add({name: 'Pikachu', height: 5, type: "electric"});
+
+pokemonRepository.add('Cow');
+
+Object.keys(pokemonRepository).forEach(function(property) {
+console.log(pokemonRepository[property]);
+});
+
+pokemonRepository.getAll().forEach(function (trait) {
+  if (trait.height > 10) {
+    document.write(
+      trait.name +
+        ' (height: ' +
+        trait.height +
+        ') - ' +
+        "Wow, that's big!" +
+        '<br><br>'
+    );
+  }
+  else document.write(
+    trait.name +
+    ' (height: ' +
+    trait.height + ')' +
+    '<br><br>');
+});
